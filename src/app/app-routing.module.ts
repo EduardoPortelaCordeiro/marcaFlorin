e07c06949d11bcc48fbbfeac9c7a4e0b06409f34
Eduardo <eduardocordeirosites@gmail.com>
2022-07-12 16:44:01 -0300

    marca florin second commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { HomeComponent } from './home/home.component';
+import { NgModule } from "@angular/core";
+import { Router, RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+    {path: '', component: HomeComponent }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports:[RouterModule]
+})
+
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BarraSuperiorComponent } from './barra-superior/barra-superior.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BarraSuperiorComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
