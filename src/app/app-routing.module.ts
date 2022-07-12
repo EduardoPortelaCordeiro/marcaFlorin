bc56089ee6e0fc23f7843e0d68d30d6ae16aa869
Eduardo <eduardocordeirosites@gmail.com>
2022-07-12 17:05:58 -0300

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { HomeComponent } from './home/home.component';
 import { NgModule } from "@angular/core";
-import { Router, RouterModule, Routes } from "@angular/router";
+import { RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
     {path: '', component: HomeComponent }
